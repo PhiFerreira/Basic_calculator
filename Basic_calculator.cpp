967c8393339aec69c19379b047332d412033b801
PhiFerreira <148065400+PhiFerreira@users.noreply.github.com>
2024-01-28 22:28:49 +0100

    Add files via upload

--- Basic_calculator.cpp
+++ Basic_calculator.cpp
@@ -16,45 +16,103 @@ double basic_calc(double var1, double var2, char op) {
     }
 }
 
-void controller() {
-    static double var1{};
-    static double var2{};
-    static double var3{};
-    static char op1{};
-    static char op2{};
-    static char userInput{};
+double continue_operation(double* var1, double* var2, double* var3) {
+    char localOp{};
+    double localVar1;
+    double localVar2;
+    double localResult;
 
-    std::cout << ("Calculator by PhiFer\n\n\n");
-
-    std::cout << ("Enter first variable: ");
-    std::cin >> var1;
+    localVar1 = *var1;
 
     std::cout << ("Enter operation( +, -, *, / ): ");
-    std::cin >> op1;
+    std::cin >> localOp;
 
-    std::cout << ("Enter second variable: ");
-    std::cin >> var2;
+    std::cout << ("Enter variable: ");
+    std::cin >> localVar2;
+
+    localResult = basic_calc(localVar1, localVar2, localOp);
+    std::cout << "The result is: " << localResult << "\n";
+
+    return 0;
+}
+
+void start_operation(char status, int* keepGoing) {
+    double var1{};
+    double var2{};
+    double var3{};
+    char op1{};
+    char op2{};
+    char userInput{};
+    bool controlQuestion{};
+    if (status == 's') {
+        std::cout << ("Enter first variable: ");
+        std::cin >> var1;
+
+        std::cout << ("Enter operation( +, -, *, / ): ");
+        std::cin >> op1;
+
+        std::cout << ("Enter second variable: ");
+        std::cin >> var2;
 
 
-    var3 = basic_calc(var1, var2, op1);
+        var3 = basic_calc(var1, var2, op1);
 
-    std::cout << "The result is: " << var3 << "\n" << "Do you want to continue calculations with this result?(y/n): ";
+        std::cout << "The result is: " << var3 << "\n" << "Do you want to continue calculations with this result?(y/n): ";
+        std::cin >> userInput;
+
+        if (userInput == 'y') {
+            var1 = var3;
+            continue_operation(&var1, &var2, &var3);
+        }
+    }
+    else if (status == 'a') {
+        std::cout << "This function is not implemented yet. Arriving soon........";
+    }
+
+    controlQuestion = true;
+    while (controlQuestion) {
+        std::cout << "Do you want to quit? (n/y): ";
+        std::cin >> userInput;
+        if (userInput == 'y') {
+            *keepGoing = 0;
+            controlQuestion = false;
+        }
+        else if (userInput == 'n') {
+            std::cout << "Starting new session...\n";
+            controlQuestion = false;
+        }
+        else {
+            std::cout << "Invalid input. Try again";
+        }
+    }
+}
+
+void controller(int* keepGoing) {
+    
+    char userInput{};
+    std::cout << ("Calculator by PhiFer\n\n\n");
+    std::cout << ("Do you need simple or advanced calculator? (s/a): ");
+    std::cin >> userInput;
+    
+    if (userInput == 's' || userInput == 'a') {
+        start_operation(userInput, keepGoing);
+    }
+    else {
+        //Legge inn en while funskjon i starten for å evt. resette.
+    }
 
 }
 
-void start_operation() {
 
 
-}
 
-void continue_operation() {
-
-
-}
 
 int main()
 {
-    controller();
+    int keepGoing{ 1 };
+    while (keepGoing) {
+        controller(&keepGoing);
+    }
 
     
     
